f7587797351d0204849f4515b0ce788cfcb5346e
Taku Kitamura <tikutaku0128@icloud.com>
2018-06-30 11:50:20 +0900

    fixx

--- MessageBoard.java
+++ MessageBoard.java
@@ -202,7 +202,8 @@ public class MessageBoard extends Application {
         "-fx-border-style: solid inside;" +
         "-fx-border-width: 2;" +
         "-fx-border-insets: 5;" +
-        "-fx-border-radius: 5;");
+        "-fx-border-radius: 5;" +
+        "-fx-border-color: white;");
 
     Scene scene = new Scene(pane);
 
